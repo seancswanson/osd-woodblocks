3054b4dd3b6057b8c62462bba1301597c8608b48
Sean Swanson <sean-swan@hotmail.com>
2018-12-29 22:50:32 -0800

    Redesigned app page. Added About section. Some JS reorg.

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 
 Explore these historic woodblock prints by legendary Japanese Artists.
 Artwork sourced from the Metropolitan Museum of Art open collection.
+
+
+## Sources
+
+Hokusai Bio: http://www.artnet.com/artists/katsushika-hokusai/biography
+Hokusai Portrait: https://upload.wikimedia.org/wikipedia/commons/f/f1/Portrait_of_Hokusai_by_Keisai_Eisen.jpg
+Hokusai Collage: https://cdn-images-1.medium.com/max/1600/1*E0KjmYv2OxY-Xtt_broYNg.jpeg
+Artwork: The Metropolitan Museum of Art in New York, New York - https://www.metmuseum.org/
--- public/js/client.js
+++ public/js/client.js
@@ -2,11 +2,66 @@
   //----------
   var mainSection = document.querySelector('main');
   var introSection = document.querySelector('.intro');
+  var aboutSection = document.querySelector('.about-section')
+  var bioSection = document.querySelector('.bio-section')
+  var indexSection = document.querySelector('.index-section')
+  var introImage = document.querySelector('.intro-image');
+  console.log(introImage);
+  var mobileSize = window.matchMedia('(max-width: 600px)')
   var openSeadragonViewer = document.querySelector('#openseadragon');
+
+  var introNavButtons = [{
+    key: 'about',
+    el: document.querySelector('#about-btn')
+  },
+  {
+    key: 'bio',
+    el: document.querySelector('#bio-btn')
+  },
+  {
+    key: 'index',
+    el: document.querySelector('#index-btn')
+  }
+];
 
   window.App = {
     init: function() {
       this.openSeadragonShown = false;
+      this.introSection = 'about';
+      // mobileSize.onchange = App.adjustIntroImage(App.introSection);
+      console.log(mobileSize);
+
+      introNavButtons.forEach(function(button) {
+        console.log(button.el);
+        button.el.addEventListener('click', function() {
+          if (button.key === 'about') {
+            this.introSection = 'about';
+            aboutSection.classList.remove('hidden');
+            bioSection.classList.add('hidden');
+            indexSection.classList.add('hidden');
+            introImage.classList.remove('hidden');
+            introImage.classList.add('intro-collage');
+            introImage.classList.remove('intro-portrait');
+            introSection.style.gridTemplateColumns = '';
+          } else if (button.key === 'bio') {
+            this.introSection = 'bio';
+            aboutSection.classList.add('hidden');
+            bioSection.classList.remove('hidden');
+            indexSection.classList.add('hidden');
+            introImage.classList.remove('hidden');
+            introImage.classList.remove('intro-collage');
+            introImage.classList.add('intro-portrait');
+            introSection.style.gridTemplateColumns = '';
+          } else if (button.key === 'index') {
+            this.introSection = 'index';
+            aboutSection.classList.add('hidden');
+            bioSection.classList.add('hidden');
+            indexSection.classList.remove('hidden');
+            introImage.classList.add('hidden');
+            introSection.style.gridTemplateColumns = '1fr';
+          }
+        });
+      });
     },
     toggleOpenSeadragon: function() {
       var self = this;
@@ -26,8 +81,24 @@
         openSeadragonViewer.style.display = 'none';
         viewer.destroy();
       }
+    },
+    adjustIntroImage: function(section) {
+      if (mobileSize.matches) {
+        console.log('match', mobileSize, arguments);
+        if (section === 'about') {
+          introImage.style.backgroundPosition = 'center';
+          introImage.style.backgroundSize = '150%';
+        }
+      } else {
+        console.log('not ', mobileSize, arguments);
+        if (section === 'about') {
+          introImage.style.backgroundPosition = 'top left';
+          introImage.style.backgroundSize = '400%';
+        }
+      }
     }
   }
 
   App.init();
+
 })();
